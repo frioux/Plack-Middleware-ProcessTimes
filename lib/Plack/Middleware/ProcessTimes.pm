131f678aa11f375f4f226db6b528ee530ace4a63
Randal L. Schwartz <merlyn@stonehenge.com>
2015-08-14 08:48:46 -0700

    Actual working code

--- lib/Plack/Middleware/ProcessTimes.pm
+++ lib/Plack/Middleware/ProcessTimes.pm
@@ -10,10 +10,32 @@ use parent 'Plack::Middleware';
 sub call {
   my ($self, $env) = @_;
 
+  my @times = (time, times);
+
   my $res = $self->app->($env);
 
   return $self->response_cb($res, sub{
     my $inner = shift;
+
+    ## reap any children so child CPU is correct
+    # 1 while waitpid(-1, 1) > 0;
+    ## when commented out:
+    ## overall end-to-end times and performance are better
+    ## but the child-user and child-sys times will always be 0
+
+    ## compute delta
+    @times = map { $_ - shift @times } time, times;
+
+    my $CPU = 0;
+    $CPU += $times[$_] for 1..4;
+    push @times, $CPU;
+
+    @times = map { sprintf "%.3f", $_ } @times;
+
+    #DEBUG# warn "times: @times\n"; 	# for now
+
+    ## make these values available to the log formatter:
+    @$env{qw( HTTP_REAL HTTP_CPUUSER HTTP_CPUSYS HTTP_CPUCUSER HTTP_CPU )} = @times;
 
     return;
   });
